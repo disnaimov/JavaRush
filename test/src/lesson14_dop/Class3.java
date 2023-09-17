cbe2f842a4a6af42c93b43a0ec2d08da94045d45
disnaimov <dimitriy.kiselev0@mail.ru>
2023-09-18 00:12:28 +0400

    Решить задачку уровня 14

--- test/src/lesson14_dop/Class3.java
+++ test/src/lesson14_dop/Class3.java
@@ -2,21 +2,46 @@ package lesson14_dop;
 
 import java.util.Scanner;
 
+/*
+Наибольший общий делитель (НОД).
+Ввести с клавиатуры 2 целых положительных числа.
+Вывести в консоль наибольший общий делитель
+ */
 public class Class3 {
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a % 2;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input two numbers");
 
-        int a = 0;
-        int b = 0;
+        int a, b;
 
-        String line = scanner.nextLine();
+        while (true) {
+            String s = scanner.nextLine();
+            if (s.matches("\\d+")) {
+                a = Integer.parseInt(s);
+                break;
+            }
+        }
 
-        a = Integer.parseInt(line);
+        while (true) {
+            String s = scanner.nextLine();
+            if (s.matches("\\d+")) {
+                b = Integer.parseInt(s);
+                break;
+            }
+        }
 
+        scanner.close();
 
-        System.out.println(a);
-        System.out.println(b);
+        System.out.println(gcd(a, b));
     }
 }
