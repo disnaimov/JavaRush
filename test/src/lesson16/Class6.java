c334337344c7202154d11f9ec0ab787844cbcef8
disnaimov <dimitriy.kiselev0@mail.ru>
2023-09-28 23:13:15 +0400

    Решить пару задачек уровня 16

---
+++ test/src/lesson16/Class6.java
@@ -0,0 +1,33 @@
+package lesson16;
+
+public class Class6 {
+
+    static class printStr implements Runnable{
+        public void run() {
+            int i = 0;
+            while (i < 20){
+                System.out.println("fistThread string " + i);
+                i++;
+            }
+        }
+    }
+
+    static class printStr2 implements Runnable{
+        public void run(){
+            int i = 0;
+            while (i < 20){
+                System.out.println("secondThread string " + i);
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new printStr());
+        Thread thread1 = new Thread(new printStr2());
+
+        thread.start();
+        thread1.start();
+    }
+
+}
