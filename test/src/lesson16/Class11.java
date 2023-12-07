99db4bb2f83d4c73f17c5d3820c38625a831f6e9
disnaimov <dimitriy.kiselev0@mail.ru>
2023-12-07 21:45:38 +0400

    Решить задачку уровня 16

--- test/src/lesson16/Class11.java
+++ test/src/lesson16/Class11.java
@@ -1,4 +1,9 @@
 package lesson16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  1. Часы
 
@@ -8,5 +13,32 @@ package lesson16;
 2.2. Через вторые полсекунды должна выводиться в консоль фраза: Tak.
  */
 public class Class11 {
+
+    public static void printTikTak() throws InterruptedException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try{
+            String str = "";
+            while (true) {
+                if (str.isEmpty()) {
+                    str = reader.readLine();
+                System.out.println("Tik");
+                Thread.sleep(500);
+                System.out.println("Tak");
+                } else if (! str.isEmpty()){
+                    break;
+                }
+                
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        printTikTak();
+
+    }
     
 }
